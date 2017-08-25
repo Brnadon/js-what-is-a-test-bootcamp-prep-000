ccc20fa8c93422d77eeabd4f20e47bf51c1a287c
Brandon Brasson <rocksong622@gmail.com>
2017-08-25 16:21:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Joe"
-var height = "74"
+var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
@@ -9,4 +9,4 @@ index.height
 index.name
 
 console.log(message)
-console.log(message) 
+console.log(message)
